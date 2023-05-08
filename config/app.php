92ee8d76ddb8b72a907391aee7e7154f7692d71c
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2023-05-08 17:06:36 +0700

    add auth api

--- config/app.php
+++ config/app.php
@@ -174,6 +174,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
+        App\Providers\JetstreamServiceProvider::class,
+
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
+        // Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
     ],
 
